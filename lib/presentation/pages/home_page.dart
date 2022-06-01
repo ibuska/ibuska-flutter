2eb9c5c1384028611add8d52ab8d4a8aaf84dca6
Muhammad Athallah <mhd.athallah@gmail.com>
2022-06-02 06:48:31 +0700

    Add pages

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:ibuska/presentation/widgets/drawer_menu.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+  static const routeName = '/home';
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -18,14 +20,51 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    const routeName = '/home';
+    var screenSize = MediaQuery.of(context).size;
+    var screenHeight = screenSize.height;
+    var screenWidth = screenSize.width;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Ibuska"),
+        title: const Text(
+          "IBUSKA",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
+      drawer: const DrawerMenu(routeName),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SizedBox(
+              width: screenWidth * 0.9,
+              height: screenHeight * 0.05,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(36.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFF6DB00),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black12,
+                          offset: Offset(3.0, 6.0),
+                          blurRadius: 10.0),
+                    ],
+                  ),
+                  child: Text(
+                    "Informasi Jalur Bus",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: screenHeight * 0.04,
+                    ),
+                  ),
+                ),
+              ),
+            ),
             const Text(
               'You have pushed the button this many times:',
             ),
