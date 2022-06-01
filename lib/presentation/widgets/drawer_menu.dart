6d0ac32d21cf30a247cd5b8f3f945a5f131d0e1e
Muhammad Athallah <mhd.athallah@gmail.com>
2022-06-02 06:48:44 +0700

    Add drawer widget

---
+++ lib/presentation/widgets/drawer_menu.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:ibuska/presentation/pages/home_page.dart';
+
+class DrawerMenu extends StatelessWidget {
+  final String route;
+
+  const DrawerMenu(this.route, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {    
+    return Drawer(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            // ignore: sized_box_for_whitespace
+            Container(
+              width: double.infinity,
+              height: 64,
+              child: const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color(0xFFF6DB00),
+                ),
+                child: Text(
+                  'Informasi Bus Kampus',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children: <Widget>[
+                  ListTile(
+                    title: const Text('Beranda'),
+                    onTap: () {
+                      route == HomePage.routeName
+                        ? Navigator.pop(context)
+                        : Navigator.pushReplacementNamed(context, HomePage.routeName);
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('Posisi Bus'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    title: const Text('Jadwal Bus'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    title: const Text('Rute Bus'),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    title: const Text('Kontak Kami'),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+  }
+}
