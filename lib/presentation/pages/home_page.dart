6bc8afc510426976dc281c0512a5f8c2200a8915
Muhammad Athallah <mhd.athallah@gmail.com>
2022-06-04 07:11:01 +0700

    Add bus general info

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -29,30 +29,83 @@ class _HomePageState extends State<HomePage> {
       drawer: const DrawerMenu(HomePage.routeName),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            SizedBox(
-              width: screenWidth * 0.85,
-              height: screenHeight * 0.05,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(36.0),
-                child: Container(
-                  alignment: Alignment.center,
-                  decoration: const BoxDecoration(
-                    color: Color(0xFFF6DB00),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black12,
-                          offset: Offset(3.0, 6.0),
-                          blurRadius: 10.0),
-                    ],
+            Padding(
+              padding: EdgeInsets.only(
+                right: screenWidth * 0.075,
+                left: screenWidth * 0.075,
+                top: screenHeight * 0.04,
+                bottom: screenHeight * 0.01,
+              ),
+              child: SizedBox(
+                width: screenWidth * 0.85,
+                height: screenHeight * 0.3,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    alignment: Alignment.topCenter,
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(125, 246, 219, 0),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: screenHeight * 0.015,
+                          ),
+                          child: Text(
+                            "Informasi Bus Kampus",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: screenHeight * 0.035,
+                            ),
+                          ),
+                        ),
+                        const Divider(),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: screenHeight * 0.01,
+                          ),
+                          child: Text(
+                            "Jam Operasional: 07.20 - 17.00 WIB",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: screenHeight * 0.035,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  child: Text(
-                    "Informasi Rute Bus",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: screenHeight * 0.035,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: screenHeight * 0.01,
+                bottom: screenHeight * 0.01,
+              ),
+              child: SizedBox(
+                width: screenWidth * 0.85,
+                height: screenHeight * 0.05,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(36.0),
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFF6DB00),
+                    ),
+                    child: Text(
+                      "Informasi Rute Bus",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: screenHeight * 0.035,
+                      ),
                     ),
                   ),
                 ),
