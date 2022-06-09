d55dcce839a05a12cd102bcb96ebd62af9ba5012
Muhammad Athallah <mhd.athallah@gmail.com>
2022-06-10 04:47:58 +0700

    Add two first bus data (?)

---
+++ lib/data/catalogues/bus_data.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:ibuska/data/models/bus.dart';
+
+Bus lurus1 = const Bus(
+  kode: "LURUS-1",
+  warna: "merah",
+  jamKeberangkatan: TimeOfDay(hour: 7, minute: 00),
+);
+
+Bus belok1 = const Bus(
+  kode: "BELOK-1",
+  warna: "biru",
+  jamKeberangkatan: TimeOfDay(hour: 7, minute: 10),
+);
